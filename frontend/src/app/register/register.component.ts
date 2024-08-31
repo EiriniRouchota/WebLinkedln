e3d556cd4f21f4a56424006df6c9d3f2f0c58a5d
irene_rouh <st1059654@ceid.upatras.gr>
2024-08-31 14:23:13 +0300

    fix message email exists

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -5,67 +5,84 @@ import { AlertService } from '../services/alert.service'; // Import the AlertSer
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'] // corrected styleUrls to plural
+  styleUrls: ['./register.component.scss'], // corrected styleUrls to plural
 })
-
 export class RegisterComponent {
-
-  employeename: string = "";
-  lastname: string = "";
-  email: string = "";
-  phone: string = "";
-  password: string = "";
-  confirmPassword: string = "";
+  employeename: string = '';
+  lastname: string = '';
+  email: string = '';
+  phone: string = '';
+  password: string = '';
+  confirmPassword: string = '';
   alertMessage: string = '';
-  alertType: string = ''; 
+  alertType: string = '';
 
-
-
-    constructor(private http: HttpClient, private alertService: AlertService) { } // Inject both services
+  constructor(private http: HttpClient, private alertService: AlertService) {} // Inject both services
 
   save(registerForm: NgForm) {
     // Check if the form is valid
     if (!registerForm.valid) {
-      this.alertService.showAlert("danger","Please fill out all required fields correctly.");
+      this.alertService.showAlert(
+        'danger',
+        'Please fill out all required fields correctly.'
+      );
       return;
     }
-  
+
     // Check if passwords match
     if (this.confirmPassword !== this.password) {
-      this.alertService.showAlert("danger","Password and confirm password fields must match!");
+      this.alertService.showAlert(
+        'danger',
+        'Password and confirm password fields must match!'
+      );
       return;
     }
-  
+
     // If form is valid and passwords match, proceed with the HTTP request
     let bodyData = {
-      "employeename": this.employeename,
-      "email": this.email,
-      "password": this.password,
-      "employeelastname": this.lastname,
-      "phone": this.phone
+      employeename: this.employeename,
+      email: this.email,
+      password: this.password,
+      employeelastname: this.lastname,
+      phone: this.phone,
     };
-  
-    this.http.post("http://localhost:8080/api/v1/employee/save", bodyData, { responseType: "text" })
+
+    this.http
+      .post('http://localhost:8080/api/v1/employee/save', bodyData, {
+        responseType: 'text',
+      })
       .subscribe({
         next: (resultData: any) => {
           console.log(resultData);
-          if (resultData === "An employee with this email already exists.") {
-            this.alertService.showAlert("warning","An employee with this email already exists.");
+          if (
+            resultData === 'An employee with this email already exists.' ||
+            resultData == ''
+          ) {
+            this.alertService.showAlert(
+              'warning',
+              'An employee with this email already exists.'
+            );
           } else {
-            this.alertService.showAlert("success","Employee Registered Successfully");
+            this.alertService.showAlert(
+              'success',
+              'Employee Registered Successfully'
+            );
           }
         },
         error: (error) => {
-          console.error("There was an error during the registration process", error);
+          console.error(
+            'There was an error during the registration process',
+            error
+          );
           if (error.status === 400) {
-            alert("Bad Request. Please check the data you have entered.");
-          } else if (error.status === 409) {  // Assuming 409 for conflict (email already exists)
-            alert("An employee with this email already exists.");
+            alert('Bad Request. Please check the data you have entered.');
+          } else if (error.status === 409) {
+            // Assuming 409 for conflict (email already exists)
+            alert('An employee with this email already exists.');
           } else {
-            alert("An unexpected error occurred. Please try again later.");
+            alert('An unexpected error occurred. Please try again later.');
           }
-        }
+        },
       });
   }
 }
-  
